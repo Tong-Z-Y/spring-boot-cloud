5474c25e182a947c933c331691472b10aa68d4c2
TZY <18789432816@126.com>
2024-05-18 00:46:54 +0800

    问题修复

--- spring-boot-business/spring-boot-fs/src/main/java/cn/com/tzy/springbootfs/service/freeswitch/SendAgentMessageImpl.java
+++ spring-boot-business/spring-boot-fs/src/main/java/cn/com/tzy/springbootfs/service/freeswitch/SendAgentMessageImpl.java
@@ -5,18 +5,11 @@ import cn.com.tzy.springbootstarterfreeswitch.enums.fs.AgentStateEnum;
 import cn.com.tzy.springbootstarterfreeswitch.model.fs.AgentVoInfo;
 import cn.com.tzy.springbootstarterfreeswitch.model.notice.CallMessage;
 import cn.com.tzy.springbootstarterfreeswitch.service.freeswitch.SendAgentMessage;
-import com.corundumstudio.socketio.BroadcastOperations;
-import com.corundumstudio.socketio.SocketIONamespace;
-import com.corundumstudio.socketio.SocketIOServer;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 @Service
 public class SendAgentMessageImpl implements SendAgentMessage {
 
-    @Resource
-    private SocketIOServer socketIOServer;
 
     @Override
     public void sendMessage(AgentStateEnum agentStateEnum, AgentVoInfo agentVoInfo, CallMessage callMessage) {
@@ -25,13 +18,6 @@ public class SendAgentMessageImpl implements SendAgentMessage {
 
     @Override
     public void sendMessage(String namespace,String agentCommon,String agentCode, RestResult result) {
-        String format = String.format("%s:%s", namespace, agentCode);
-        SocketIONamespace socketIONamespace = socketIOServer.getNamespace(namespace);
-        if(socketIONamespace != null){
-            BroadcastOperations roomOperations = socketIONamespace.getRoomOperations(format);
-            if(roomOperations !=null){
-                roomOperations.sendEvent(agentCommon,result);
-            }
-        }
+
     }
 }
